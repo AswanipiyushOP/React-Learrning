a6f2557812c707731cf6d8c8d49956f947838d11
AswanipiyusOP <aswanipiyush1234@gmail.com>
2024-11-06 19:50:40 +0530

    second commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Cards from "./cards";
 
+
+
 const MainContent = () => {
+//   const [isGoingOut , setIsGoingOut] = useState(true);
+// const [thingsArray, setThingsArray] = useState(["Thing 1" , "Thing 2"]);
+// function handleClick()
+// {
+//   setIsGoingOut(prevState => !prevState)
+// }
+// function addItem(){
+//   setThingsArray(prevThingsArray => {
+//     return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
+//   })
+// }
+// const thingsElement = thingsArray.map(thing => <p key={thing}>{thing}</p>)
   return (
     <div>
-      <h2 className="font-serif pt-4 pl-4">Main content goes here</h2>
+      {/* <div onClick={handleClick}>
+        <h2 className="font-serif pt-4 pl-4">Is i'm going out today?</h2>
+        <p>{isGoingOut ? "Yes" : "No"}</p>
+      </div> */}
+      {/* <div>
+        <button onClick={addItem}>
+          Add Item
+        </button>
+        {thingsElement}
+      </div> */}
       <div className="justify-items-center">
         <Cards />
       </div>
